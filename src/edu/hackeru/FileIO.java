8926791cb4a956fd12b97c638ec5cd916d6a5c28
hackerudroids <tomerjava@gmail.com>
2017-03-16 17:39:18 +0200

    Initial commit

---
+++ src/edu/hackeru/FileIO.java
@@ -0,0 +1,56 @@
+package edu.hackeru;
+
+import java.io.*;
+
+/**
+ * Created by hackeru on 13/03/2017.
+ */
+public class FileIO {
+
+    public static String read(String file){
+        BufferedReader reader = null;
+        try {
+            StringBuilder builder = new StringBuilder();
+            reader = new BufferedReader(new FileReader(file));
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                builder.append(line);
+            }
+            return builder.toString();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+          close(reader);
+        }
+        return null;
+    }
+
+    private static void close(Closeable closable) {
+        if (closable != null) {
+            try {
+                closable.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void write(String file, String data){
+       write(file, data, false);
+    }
+
+    public static void write(String file, String data, boolean append){
+        FileWriter fileWriter = null;
+        try {
+            fileWriter = new FileWriter(file, append);
+            fileWriter.write(data);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            close(fileWriter);
+        }
+    }
+}
