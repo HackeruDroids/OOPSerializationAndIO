f4f9f96574391e91955aeb50829791231b8950bf
hackerudroids <tomerjava@gmail.com>
2017-03-16 18:39:33 +0200

    Person Serialization

--- src/edu/hackeru/Main.java
+++ src/edu/hackeru/Main.java
@@ -3,7 +3,11 @@ package edu.hackeru;
 public class Main {
 
     public static void main(String[] args) {
+        Person p = new Person("Moshe", "Doe");
 
+//        FileIO.write("people.txt", p.getFirstName(), true);
+//        FileIO.write("people.txt", ";", true);
+//        FileIO.writeln("people.txt", p.getLastName(), true);
 
 
     }
--- src/edu/hackeru/Person.java
+++ src/edu/hackeru/Person.java
@@ -1,5 +1,7 @@
 package edu.hackeru;
 
+import com.sun.istack.internal.NotNull;
+
 public class Person {
     private String firstName;
     private String lastName;
@@ -26,4 +28,28 @@ public class Person {
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+
+    public String toCSV() {
+        return String.format("%s;%s%s",
+                firstName, lastName, System.lineSeparator()
+        );
+    }
+
+    //Factory method
+    public static Person fromCSV(@NotNull String csv ){
+        String[] p = csv.split(";");
+        if (p.length !=2)
+            throw new RuntimeException("Bad format");
+        return new Person(p[0], p[1]);
+    }
+
+
 }
